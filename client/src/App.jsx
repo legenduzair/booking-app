b7892afa615c47a27adcf848d5647b3e16884781
Uzair Oomer <uzair.oomer@jdplc.com>
2023-11-13 20:55:43 +0000

    reconfigure component structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
 
-import Header from './components/Header'
 import IndexPage from './pages/IndexPage'
+import LoginPage from './pages/LoginPage'
 
 import './App.css'
-
 
 function App() {
   return (
     <Routes>
       <Route index element={<IndexPage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   )
 }
---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>Login page should go here</div>
+  )
+}
+
+export default LoginPage
