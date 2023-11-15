16a9a577652cf7318ea12ad9d9744f8cf98d643d
Uzair Oomer <uzair.oomer@jdplc.com>
2023-11-15 15:50:19 +0000

    Implement login page and styling. Add react routers for nav linking

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,15 @@ import IndexPage from './pages/IndexPage'
 import LoginPage from './pages/LoginPage'
 
 import './App.css'
+import Layout from './Layout'
 
 function App() {
   return (
     <Routes>
-      <Route index element={<IndexPage />} />
-      <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<Layout/>}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Route>
     </Routes>
   )
 }
--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -4,7 +4,7 @@ import Header from '../components/Header'
 const IndexPage = () => {
   return (
     <div>
-      <Header />
+      Index Page
     </div>
   )
 }
