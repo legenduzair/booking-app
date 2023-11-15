4d4cdefc40537ea4a65cfc76325342f165c793bd
Uzair Oomer <uzair.oomer@jdplc.com>
2023-11-15 15:57:06 +0000

    Add front end for register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 
 import IndexPage from './pages/IndexPage'
 import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
 
 import './App.css'
 import Layout from './Layout'
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<Layout/>}>
         <Route index element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
     </Routes>
   )
