c0499cb3889e33f36783d93c5f0d24e87b8006fa
Uzair Oomer <uzair.oomer@jdplc.com>
2023-11-20 14:43:02 +0000

    Add perks in separate component and add states for each form field

--- api/models/Place.js
+++ api/models/Place.js
@@ -10,7 +10,7 @@ const placeSchema = new mongoose.Schema({
     extraInfo: String,
     checkIn: Number,
     checkOut: Number,
-    maxGuests: Number,
+    maxGuests: String,
 });
 
 const PlaceModel = mongoose.model('Place', placeSchema);
