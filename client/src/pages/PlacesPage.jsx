7b03c62d46e689d1b1fd68af5634d7329105d668
Uzair Oomer <uzair.oomer@jdplc.com>
2023-11-24 14:12:47 +0000

    Add functionality to upload multiple photos by file input

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -49,6 +49,20 @@ const PlacesPage = () => {
     setPhotoLink('');
   }
 
+  async function uploadPhoto(ev) {
+    const files = ev.target.files;
+    const data = new FormData();
+    for (let i = 0; i < files.length; i++) {
+      data.append('photos', files[i]);
+    }
+    const {data:filenames} = await axios.post('/upload', data, {
+      headers: {'Content-type':'multipart/form-data'}
+    });
+    setAddedPhotos(prev => {
+      return [...prev, ...filenames];
+    });
+  }
+
   return (
     <div>
       {action !== 'new' && (
@@ -77,16 +91,17 @@ const PlacesPage = () => {
                   </div>
                   <div className='mt-2 gap-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6'>
                     {addedPhotos.length > 0 && addedPhotos.map(link => (
-                      <div>
-                        <img className='rounded-2xl' src={'http://localhost:4000/uploads/' + link} alt="" />
+                      <div className='h-32 flex'>
+                        <img className='w-full object-cover rounded-2xl' src={'http://localhost:4000/uploads/' + link} alt="" />
                       </div>
                     ))}
-                      <button className='flex items-center justify-center gap-2 border bg-transparent rounded-2xl text-xl text-gray-600 p-2'>
+                      <label className='h-32 cursor-pointer flex items-center justify-center gap-2 border bg-transparent rounded-2xl text-xl text-gray-600 p-2'>
+                        <input multiple type="file" className='hidden' onChange={uploadPhoto} />
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                           <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
                         </svg>
                         Upload
-                      </button>
+                      </label>
                   </div>
                   {preInput('Description', 'Description of your place.')}
                   <textarea value={description} 
