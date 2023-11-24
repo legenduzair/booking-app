6069b076b40e34dab74b08b51d31bedb4ac169cc
Uzair Oomer <uzair.oomer@jdplc.com>
2023-11-24 14:40:39 +0000

    Fix console errors

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -97,8 +97,8 @@ const PlacesPage = () => {
                     ))}
                       <label className='h-32 cursor-pointer flex items-center justify-center gap-2 border bg-transparent rounded-2xl text-xl text-gray-600 p-2'>
                         <input multiple type="file" className='hidden' onChange={uploadPhoto} />
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                          <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
                         </svg>
                         Upload
                       </label>
