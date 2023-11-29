7b39e9f37d8a8df59a7be53909960fc0ed53b013
Uzair Oomer <uzair.oomer@jdplc.com>
2023-11-29 19:48:28 +0000

    add functionality to hide button if no photos

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -34,7 +34,7 @@ const PlacePage = () => {
                 </div>
                 {place?.photos?.length > 0 && place.photos.map(photo => (
                     <div>
-                        <img src={'http://localhost:4000/uploads/' + place.photo} alt="" />
+                        <img src={'http://localhost:4000/uploads/' + photo} alt="" />
                     </div>
                 ))}
             </div>
@@ -72,12 +72,14 @@ const PlacePage = () => {
                     </div>
                 </div>
             </div>
-            <button onClick={() => setShowAllPhotos(true)} className='flex gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow shadow-md shadow-gray-500'>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-              </svg>
-              Show More Photos
-            </button>
+            {place.photos.length > 3 && (
+                <button onClick={() => setShowAllPhotos(true)} className='flex gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow shadow-md shadow-gray-500'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                </svg>
+                Show More Photos
+              </button>
+            )}
         </div>
     </div>
   )
