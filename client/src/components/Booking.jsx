3a6998a916adda5254a94060327c5cb0bf40c88d
Uzair Oomer <uzair.oomer@jdplc.com>
2023-12-01 19:49:09 +0000

    Add script to calculate the total price in booking widget

--- client/src/components/Booking.jsx
+++ client/src/components/Booking.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { useState } from "react";
+import differenceInCalendarDays from 'date-fns/differenceInCalendarDays';
 
 const Booking = ({place}) => {
+  const [checkIn, setCheckIn] = useState('');
+  const [checkOut, setCheckOut] = useState('');
+  const [numberOfGuests, setNumberOfGuests] = useState(1);
+  let numberOfNights = 0;
+  if (checkIn && checkOut) {
+    numberOfNights = differenceInCalendarDays(new Date(checkOut), new Date(checkIn));
+  }
+
   return (
     <div className="bg-white shadow p-4 rounded-2xl">
       <div className="text-2xl text-center">
@@ -10,21 +20,34 @@ const Booking = ({place}) => {
         <div className="flex">
           <div className="py-3 px-4">
             <label>Check In:</label>
-            <input type="date" />
+            <input value={checkIn} 
+                   onChange={ev => setCheckIn(ev.target.value)}
+                   type="date" />
           </div>
           <div className="py-3 px-4 border-l">
             <label>Check Out:</label>
-            <input type="date" />
+            <input value={checkOut} 
+                   onChange={ev => setCheckOut(ev.target.value)} 
+                   type="date" />
           </div>
         </div>
         <div>
           <div className="py-3 px-4 border-t">
             <label>Guests:</label>
-            <input type="number" value={1} />
+            <input value={numberOfGuests} 
+                   onChange={ev => setNumberOfGuests(ev.target.value)} 
+                   type="number" />
           </div>
         </div>
       </div>
-      <button className="primary mt-4">Book This Place</button>
+      <button className="primary mt-4">
+        Book This Place
+        {numberOfNights > 0 && (
+          <>
+            <span> £{numberOfNights * place.price}</span>
+          </>
+        )}
+      </button>
     </div>
   );
 };
